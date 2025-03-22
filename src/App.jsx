698e4b9b87930dba7bb3b59a30dce171e763eaf7
Siddharth Shah <sidshah895@gmail.com>
2025-03-22 23:46:41 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AppLayout } from "./components/layout/AppLayout";
 import "./App.css";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Country } from "./pages/Country";
+import { ErrorPage } from "./pages/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/country",
+        element: <Country />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <div>atlas-world project</div>
-    </>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
