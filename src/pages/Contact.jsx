70eea10a9613a6919ed08a906f5a2785e4d49271
Siddharth Shah <sidshah895@gmail.com>
2025-03-26 21:38:48 +0530

    loader,Contact_page,Country_page,CountryCard and CountryDetails page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,3 +1,42 @@
 export const Contact = () => {
-  return <div>Contact</div>;
+  const handleFormSubmit = (formData) => {
+    const formInputData = Object.fromEntries(formData.entries());
+  };
+
+  return (
+    <section className="section-contact">
+      <h1 className="container-title">Contact Us</h1>
+      <div className="container contact-wrapper">
+        <form action={handleFormSubmit}>
+          <input
+            type="text"
+            className="form-control"
+            autoComplete="false"
+            placeholder="Enter your name"
+            name="username"
+            required
+          />
+          <input
+            type="email"
+            className="form-control"
+            autoComplete="false"
+            placeholder="Enter your email"
+            name="email"
+            required
+          />
+          <textarea
+            className="form-control"
+            rows="10"
+            autoComplete="false"
+            placeholder="Enter your message"
+            name="message"
+            required
+          />
+          <button type="submit" value="send">
+            Send
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,3 +1,28 @@
+import { useEffect, useState, useTransition } from "react";
+import { getCountryData } from "../api/postApi";
+import { Loader } from "../components/ui/Loader";
+import { CountryCard } from "../components/layout/CountryCard";
+
 export const Country = () => {
-  return <div>Country</div>;
+  const [ispending, startTransition] = useTransition();
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    startTransition(async () => {
+      const res = await getCountryData();
+      setCountries(res.data);
+    });
+    // usetransition is a react hook taht lets you update the state without blocking the UI.
+  }, []);
+
+  if (ispending) return <Loader />;
+
+  return (
+    <section className="country-section">
+      <ul className="grid grid-four-cols">
+        {countries.map((curCountry, index) => {
+          return <CountryCard country={curCountry} key={index} />;
+        })}
+      </ul>
+    </section>
+  );
 };
